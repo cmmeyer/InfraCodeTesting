c11b078b37bfb7cb1343913a08c196415b72f71f
Chuck Meyer <cmmeyer@amazon.com>
2019-02-14 16:20:54 -0500

    Added build command to rename PDF

--- bs/build.sh
+++ bs/build.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
 
 # Build PDF deck
-md_file=presentation.md
+md_file="presentation"
 
-# bs pdf $md_file
-title=head -n 1 presentation.md | sed -En 's/title: (.*)/\1/p'
-echo($title)
+bs pdf $md_file.md
+title=`head -n 1 presentation.md | sed -En 's/title: (.*)/\1/p' | sed -e 's/[ ,\/]/\-/g'` 
+echo $title
+mv "pdf/${md_file}.pdf" "pdf/${title}.pdf"
